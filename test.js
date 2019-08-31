a3d7314aecc5de99e6042390662d84360538fd4c
Mixergi <ehdud308@naver.com>
2019-09-01 02:31:05 +0900

    asdfasdf

--- test.js
+++ test.js
@@ -1,10 +1,22 @@
-var a = {
-    apple: 'red',
-    banana: 'yellow',
-    pitch: 'pink'
-}
+Date.prototype.yyyymmddhhmmss = function() {
+    var month = this.getMonth() + 1;
+    var day = this.getDate();
+    var ymd = [this.getFullYear(),
+               (month>9 ? '' : '0') + month,
+               (day>9 ? '' : '0') + day
+              ].join('');
 
-for (var i in a) {
-    console.log(a[i]);
-}
+    var hour = this.getHours();
+    var minute = this.getMinutes();
+    var second = this.getSeconds();
   
+    var hms = [( hour >9 ? '' : '0') + hour,
+               (minute>9 ? '' : '0') + minute,
+               (second>9 ? '' : '0') + second,
+              ].join('');
+
+    return ymd+hms;
+};
+  
+  var date = new Date();
+  console.log(date.yyyymmddhhmmss());
